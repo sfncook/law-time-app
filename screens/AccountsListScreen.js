a34d7d7c34809282b1cd48f1dbfab95531be79a7
Shawn Cook <cooksha@yahoo-inc.com>
2018-10-19 14:40:19 -0700

    Format accounts list

--- screens/AccountsListScreen.js
+++ screens/AccountsListScreen.js
@@ -37,9 +37,18 @@ export default class AccountsListScreen extends React.Component {
             {key:'2', name: 'Baugh, Bo', phone:'612-222-1234', dur:'20', bal:'25'},
             {key:'3', name: 'Smith, Sally', phone:'612-333-1234', dur:'30', bal:'35'},
           ]}
-          renderItem={({item}) => <Text style={styles.acctRow}>
-            {item.name}
-            </Text>}
+          renderItem={({item}) =>
+            <View style={styles.acctRow}>
+              <View style={styles.acctRowLeftCol}>
+                <Text style={styles.std}>{item.name}</Text>
+                <Text style={styles.std}>{item.phone}</Text>
+              </View>
+              <View style={styles.acctRowRightCol}>
+                <Text style={styles.dur}>{item.dur} min</Text>
+                <Text style={styles.bal}>${item.bal}</Text>
+              </View>
+            </View>
+          }
         />
         <Button
           style={styles.addAcctBtn}
@@ -54,6 +63,13 @@ export default class AccountsListScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  flexGridThirds: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  flexGridThirdsCol: {
+    width: '32%'
+  },
   addAcctBtn: {
     backgroundColor: 'blue',
     borderColor: 'black',
@@ -64,5 +80,23 @@ const styles = StyleSheet.create({
     padding: 5,
     marginBottom: 2,
     flex: 1,
+    flexDirection: 'row',
   },
+  acctRowLeftCol: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  acctRowRightCol: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dur: {
+    margin:5,
+  },
+  bal: {
+    margin:5,
+  },
+
 });
